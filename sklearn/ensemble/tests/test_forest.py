88f8fb69593680b315f8c5d34b31b56cc60a4c3b
Simona Maggio <smaggio@Jean-Francoiss-MacBook-Pro.local>
2020-01-28 15:22:57 +0100

    Added test: prediction not outside range

--- sklearn/ensemble/tests/test_forest.py
+++ sklearn/ensemble/tests/test_forest.py
@@ -1367,3 +1367,34 @@ def test_little_tree_with_small_max_samples(ForestClass):
 
     msg = "Tree without `max_samples` restriction should have more nodes"
     assert tree1.node_count > tree2.node_count, msg
+
+def test_rf_regressor_prediction_range():
+    # Create a Random Number Generator with 42 as a fixed seed.
+    rng = np.random.RandomState(42)
+
+    # TODO: define the n_samples and n_features variables
+    n_samples = 100
+    n_features = 10
+    
+    X_train = rng.normal(size=(n_samples, n_features))
+    y_train = rng.normal(size=n_samples)
+
+    rfr = RandomForestRegressor(random_state=42)
+    rfr.fit(X_train, y_train)
+
+    # TODO: np.min() and np.max() to measure the minimum and
+    # maximum values of the training set target values y_train.
+    y_min = np.min(y_train)
+    y_max = np.max(y_train)
+
+    # TODO: generate some random data X_test with the same number
+    # of features.
+    X_test = rng.normal(size=(n_samples, n_features))
+    y_preds = rfr.predict(X_test)
+    # TODO: compute the model predictions on the test data:
+    # rfr.predict(X_test) and store the results in a variable y_preds.
+
+    # TODO: check that all the values in y_preds lie between the minimum
+    # and maximum values of y_train.
+    msg = "Tree prediction outside training range"
+    assert np.min(y_preds)>=y_min and np.max(y_preds)<=y_max, msg
